c5f1839d21b34520380124e32c05ff13ca4ee581
Sayam <sayam@oakcity.io>
2020-08-28 22:25:42 -0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
         .target(
             name: "MarkdownAttributedString",
             dependencies: [],
-            path: ""),
+            path: "Sources"),
         .testTarget(
             name: "MarkdownAttributedStringTests",
             dependencies: ["MarkdownAttributedString"]),
